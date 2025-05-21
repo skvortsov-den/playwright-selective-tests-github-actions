52f7b3d2be30e20340a15eb3f58bde0a204964f2
Denis Skvortsov <denisskvortsov@MacBook-Pro-Denis.local>
2025-05-21 11:14:26 +0200

    feat: setup GitHub Actions workflow with Playwright tests

---
+++ e2e/tests/google.spec.ts
@@ -0,0 +1,15 @@
+import { test } from '../../src/test';
+
+test.describe('Google page (RU)', () => {
+  test('Проверка наличия сервисов в сэндвич-баре', async ({ ui }) => {
+    await ui.google.goto();
+    await ui.google.openAppsMenu();
+
+    await ui.google.assertServiceVisible('YouTube');
+    await ui.google.assertServiceVisible('Youtube Music');
+    await ui.google.assertServiceVisible('Диск');
+    await ui.google.assertServiceVisible('Календарь');
+    await ui.google.assertServiceVisible('Карты');
+    await ui.google.assertServiceVisible('Почта');
+  });
+});
