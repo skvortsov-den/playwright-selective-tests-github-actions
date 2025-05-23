1ca1430a2b810ecb49f13582d6ec902f7f371ddb
Denis Skvortsov <denisskvortsov@MacBook-Pro-Denis.local>
2025-05-23 18:14:59 +0200

    final PR before release

--- tests/e2e/tests/google.spec.ts
+++ tests/e2e/tests/google.spec.ts
@@ -1,71 +1,47 @@
 import { test } from '../src/test';
 
-test.describe('Google page (RU)', () => {
-  test('Проверка наличия сервисов в сэндвич-баре', { tag: '@apps/microservice1' }, async ({ ui }) => {
+test.describe('Google page (EN)', () => {
+  test('Test 1', { tag: '@apps/microservice1' }, async ({ ui }) => {
     await ui.google.goto();
     await ui.google.openAppsMenu();
-
     await ui.google.assertServiceVisible('YouTube');
-    await ui.google.assertServiceVisible('Youtube Music');
-    await ui.google.assertServiceVisible('Диск');
-    await ui.google.assertServiceVisible('Календарь');
-    await ui.google.assertServiceVisible('Карты');
-    await ui.google.assertServiceVisible('Почта');
+    await ui.google.assertServiceVisible('YouTube Music');
+
   });
 });
 
-test.describe('Google page (RU)', () => {
+test.describe('Google page (EN)', () => {
   test('Test 2', { tag: '@apps/microservice2' }, async ({ ui }) => {
     await ui.google.goto();
     await ui.google.openAppsMenu();
-
-    await ui.google.assertServiceVisible('YouTube');
-    // await ui.google.assertServiceVisible('Youtube Music');
-    await ui.google.assertServiceVisible('Диск');
-    await ui.google.assertServiceVisible('Календарь');
-    await ui.google.assertServiceVisible('Карты');
-    await ui.google.assertServiceVisible('Почта');
+    await ui.google.assertServiceVisible('Drive');
+    await ui.google.assertServiceVisible('Calendar');
   });
 });
 
-test.describe('Google page (RU)', () => {
+test.describe('Google page (EN)', () => {
   test('Test 3', { tag: '@apps/microservice3' }, async ({ ui }) => {
     await ui.google.goto();
     await ui.google.openAppsMenu();
-
-    await ui.google.assertServiceVisible('YouTube');
-    await ui.google.assertServiceVisible('Youtube Music');
-    // await ui.google.assertServiceVisible('Диск');
-    await ui.google.assertServiceVisible('Календарь');
-    await ui.google.assertServiceVisible('Карты');
-    await ui.google.assertServiceVisible('Почта');
+    await ui.google.assertServiceVisible('Maps');
+    await ui.google.assertServiceVisible('Gmail');
   });
 });
 
-test.describe('Google page (RU)', () => {
+test.describe('Google page (EN)', () => {
   test('Test 4', { tag: '@apps/microservice4' }, async ({ ui }) => {
     await ui.google.goto();
     await ui.google.openAppsMenu();
 
     await ui.google.assertServiceVisible('YouTube');
-    await ui.google.assertServiceVisible('Youtube Music');
-    await ui.google.assertServiceVisible('Диск');
-    await ui.google.assertServiceVisible('Календарь');
-    await ui.google.assertServiceVisible('Карты');
-    // await ui.google.assertServiceVisible('Почта');
+    await ui.google.assertServiceVisible('Maps');
   });
 });
 
-test.describe('Google page (RU)', () => {
+test.describe('Google page (EN)', () => {
   test('Test 5', async ({ ui }) => {
     await ui.google.goto();
     await ui.google.openAppsMenu();
-
-    await ui.google.assertServiceVisible('YouTube');
-    await ui.google.assertServiceVisible('Youtube Music');
-    await ui.google.assertServiceVisible('Диск');
-    await ui.google.assertServiceVisible('Календарь');
-    await ui.google.assertServiceVisible('Карты');
-    await ui.google.assertServiceVisible('Почта');
+    await ui.google.assertServiceVisible('Calendar');
   });
 });
